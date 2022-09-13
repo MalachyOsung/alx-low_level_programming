eaed7bb84d3991c5aac6fa8e0528348fd8d29ae4
MalachyOsung <malachy.osung@gmail.com>
2022-09-13 16:58:13 -0700

    a - z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet : this function would print the alphabet
+ * return : 0
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+	while(ch <= 'z')
+	{
+		putchar(ch);
+		putchar("\n");
+
+	}
+
+}
