62d94605029b4e375d20c800a11725001c6cc3d1
MalachyOsung <malachy.osung@gmail.com>
2022-09-17 12:29:03 -0700

    revised

---
+++ 0x00-hello_world/testing.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ *    
+ */
+
+int main(void)
+	
+{
+	printf("Enter your name");
+    return (0);
+
+}
