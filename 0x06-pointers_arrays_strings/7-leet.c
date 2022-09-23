aa17fe36d21745bc7153965f14241572e30d6bac
MalachyOsung <malachy.osung@gmail.com>
2022-09-22 19:20:08 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,46 @@
 #include "main.h"
 
 /**
- * leet - Function that reverses the content of an array of integers.
+ * leet - leet encoder
+ * @str: string to be encoded
  *
- * @str: s is the array
- *
- * Return: Always 0.
+ * Return: address of the encoded string
  */
+
 char *leet(char *str)
 {
-	int i = 0, k;
-	char s[] = "aAeEoOtTlL";
-	char s1[] = "4433007711";
+	int i = 0;
 
-	for (; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		for (k = 0; k <= 9; k++)
-		{
-			if (s[k] == str[i])
-			{
-				str[i] = s1[k];
-			}
-		}
+		str[i] = transform(str[i]);
+		i++;
 	}
 	return (str);
 }
-Footer
 
+/**
+ * transform - helper function to map a letter with it's leet encoding
+ * @x: char to be encoded
+ *
+ * Return: the encoded char
+ */
+
+char transform(char x)
+{
+	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0' 'T'};
+	int i = 0;
+	char replacement = x;
+
+	while (i < 8)
+	{
+		if (x == mapping_low[i] || x == mapping_upper[i])
+		{
+			replacement = i + '0';
+			break;
+		}
+		i++;
+	}
+	return (replacement);
+}
